7a1ca857ffa7300f93204f9143691d8939c6fd7e
Vakhitava <vaxitos@gmail.com>
2022-06-14 16:39:08 +0200

    14.06

---
+++ data type/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let number = 4.6;
+
+console.log(-4 / 0); // - Infinity
+console.log(4 / 0); // Infinity
+console.log('string' * 9); //NAN not a number
+const persone = "Alex"; // ``бэктик??? "" ''
+const persone1 = `5`; // string! not a number
+const bool = false; //false/true boolin
+console.log(something); //=>something is not defined ошиька ссылочного типа
+let und;
+console.log * (und);
