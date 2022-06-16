9ea93f0afc13bfa5508e1e76cd0ceb712b05a588
Vakhitava <vaxitos@gmail.com>
2022-06-16 19:20:34 +0200

    Create massive.js

---
+++ data type/massive.js
@@ -0,0 +1,8 @@
+const storeName = 'magaz';
+let storeDescription;
+storeDescription = {
+    'budget': 10000,
+    employees: ['Katja', 'Vanja', 'Petja'],
+    products: { 'qqq': 999, 'www': 111 },
+    open: true
+};
